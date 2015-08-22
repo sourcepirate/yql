54ea52441b3defb5b460c3a0e93dbd8411746199
plasmashadow <plasmashadowx@gmail.com>
2015-08-22 08:37:14 +0530

    initial code

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+def readme():
+    return open("README.md", "r").read()
+
+requirements = [
+    'six',
+    'requests',
+    'urllib3'
+]
+
+setup(
+    name = 'yql',
+    packages = ['yql'],
+    version = '0.0.1',
+    long_description= readme(),
+    description = "python interface for yql",
+    author='plasmashadow',
+    author_email='plasmashadowx@gmail.com',
+    url='https://github.com/plasmashadow/yql.git',
+    license="MIT",
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+    ],
+    install_requires= requirements
+)
